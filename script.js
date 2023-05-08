0204d6558ea212e80380fd15afa4bf7206846d87
Climax Mba <climaxium150@gmail.com>
2023-05-08 16:14:34 +0100

    Add functions & variables

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function() {
+  this.info = function () {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${
       this.read ? "read" : "not read yet"
     }`;
   };
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+const defaultBook = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const bookDisplay = document.getElementById("books");
 
-console.log(theHobbit.info());
+function addBookToLibrary(Book) {
+  myLibrary.push(Book);
+}
+
+addBookToLibrary(defaultBook);
+updateDisplay();
+
+function updateDisplay() {
+  let arrLength = myLibrary.length;
+
+  for (let i = 0; i < arrLength; i++) {
+    let node = document.createElement("div");
+
+    node.innerHTML = `<div class="book" data-index="${i}">
+                    <h3>${myLibrary[i].title}</h3>
+                    <p>${myLibrary[i].info()}</p>
+                    <button class="read btn-green">Mark as read</button>
+                    <button class="btn-green">Edit</button>
+                    <button class="btn-red">Remove</button>
+                </div>
+            </div>`;
+
+    bookDisplay.appendChild(node);
+  }
+}
